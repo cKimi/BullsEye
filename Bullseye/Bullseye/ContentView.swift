652720cca31e23bac01bbbca6b249d5a66d8a069
Carlos Kimura <ckimura@gmail.com>
2023-12-13 11:30:50 -0300

    instructions label, target label, slider and hit me button added to UI

--- Bullseye/Bullseye/ContentView.swift
+++ Bullseye/Bullseye/ContentView.swift
@@ -9,13 +9,17 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Text("🎯🎯🎯\nPUT THE BULLSEYE AS CLOSE AS YOU CAN TO")
+        Text("89")
+        HStack {
+            Text("1")
+            Slider(value: .constant(50), in: 1.0...100.0)
+            Text("100")
         }
-        .padding()
+        
+        Button("Hit me") {
+            
+        }
     }
 }
 
