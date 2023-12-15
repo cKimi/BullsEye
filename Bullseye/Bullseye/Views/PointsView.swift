fb911d930f39576bb3677b1063efd9805e813f57
Carlos Kimura <ckimura@gmail.com>
2023-12-15 17:33:04 -0300

    showing a custom alert

--- Bullseye/Bullseye/Views/PointsView.swift
+++ Bullseye/Bullseye/Views/PointsView.swift
@@ -8,13 +8,23 @@
 import SwiftUI
 
 struct PointsView: View {
+    
+    @Binding var alertIsVisible: Bool
+    @Binding var sliderValue: Double
+    @Binding var game: Game
+    
     var body: some View {
+        
+        let roundedValue = Int(sliderValue.rounded())
+        let points = game.points(sliderValue: roundedValue)
+        
         VStack(spacing: 10) {
             InstructionText(text: "The slider value is")
-            BigNumberText(text: "69")
-            BodyText(text: "You scored 200 points 😻😻😻")
+            BigNumberText(text: String(roundedValue))
+            BodyText(text: "You scored \(points) points 🎉🎉🎉")
             Button {
-                // start new round
+                alertIsVisible = false
+                game.startNewRound(points: points)
             } label: {
                 RoundedImageViewStroked(systemName: "arrow.counterclockwise")
             }
@@ -29,6 +39,6 @@ struct PointsView: View {
 }
 
 #Preview {
-    PointsView()
+    PointsView(alertIsVisible: .constant(false), sliderValue: .constant(51.0), game: .constant(Game()))
         .preferredColorScheme(.dark)
 }
