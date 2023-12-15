f6045e82f39be3202a8df3eb9adfa0f519d716c4
Carlos Kimura <ckimura@gmail.com>
2023-12-15 16:55:48 -0300

    body text and button text structs created

--- Bullseye/Bullseye/Views/TextViews.swift
+++ Bullseye/Bullseye/Views/TextViews.swift
@@ -18,7 +18,7 @@ struct TextViews: View {
             .lineSpacing(4.0)
             .font(.footnote)
             .kerning(2.0)
-            
+        
             .foregroundColor(Color("TextColor"))
     }
 }
@@ -60,14 +60,46 @@ struct LabelText: View {
     }
 }
 
+struct BodyText: View {
+    
+    var text: String
+    
+    var body: some View {
+        Text(text)
+            .font(.subheadline)
+            .fontWeight(.semibold)
+            .multilineTextAlignment(.center)
+            .lineSpacing(12)
+    }
+}
+
+struct ButtonText: View {
+    
+    var text: String
+    
+    var body: some View {
+        Text(text)
+            .bold()
+            .padding()
+            .frame(maxWidth: .infinity)
+            .background(
+                Color.accentColor
+            )
+            .foregroundStyle(.white)
+            .cornerRadius(12)
+    }
+}
+
 #Preview {
     VStack {
         TextViews(text: "Instructions")
         BigNumberText(text: "999")
         SliderLabelText(text: "99")
         LabelText(text: "Score")
+        BodyText(text: "You scored 200 points 😻😻😻")
+        ButtonText(text: "Start New Round")
     }
-    
+    .padding()
 }
 
 
