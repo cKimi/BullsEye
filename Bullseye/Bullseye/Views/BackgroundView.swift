67d37671cc026e8e18faba7f793b790a146ebfb3
Carlos Kimura <ckimura@gmail.com>
2023-12-15 15:24:06 -0300

    background rings added

--- Bullseye/Bullseye/Views/BackgroundView.swift
+++ Bullseye/Bullseye/Views/BackgroundView.swift
@@ -19,8 +19,7 @@ struct BackgroundView: View {
         }
         .padding()
         .background(
-            Color("BackgroundColor")
-                .ignoresSafeArea()
+            RingsView()
         )
     }
 }
@@ -65,6 +64,27 @@ struct BottomView: View {
             NumberView(title: "Score", text: String(game.score))
             Spacer()
             NumberView(title: "Round", text: String(game.round))
+        }
+    }
+}
+
+struct RingsView: View {
+    var body: some View {
+        ZStack {
+            Color("BackgroundColor")
+                .ignoresSafeArea()
+            ForEach(1..<6) { ring in
+                let size = CGFloat(ring * 100)
+                Circle()
+                    .stroke(lineWidth: 20)
+                    .fill(
+                        RadialGradient(colors: [Color("RingsColor").opacity(0.3 * 0.8), Color("RingsColor").opacity(0)],
+                                       center: .center,
+                                       startRadius: 100,
+                                       endRadius: 300)
+                    )
+                    .frame(width: size, height: size)
+            }
         }
     }
 }
